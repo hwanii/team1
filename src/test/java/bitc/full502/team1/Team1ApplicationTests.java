fa0ecf6dadbabf43406960cb7663324761d4fb78
hwanii <rojh9018@naver.com>
2025-07-10 09:01:58 +0900

    프로젝트 시작.

---
+++ src/test/java/bitc/full502/team1/Team1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.full502.team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
